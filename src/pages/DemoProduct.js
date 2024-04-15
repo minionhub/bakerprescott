ca34660888e82028b803a05bd547cbc5224a59bc
minionhub.dev <minionhub.dev@gmail.com>
2024-04-15 10:41:58 -0500

    change title

--- src/pages/DemoProduct.js
+++ src/pages/DemoProduct.js
@@ -8,7 +8,7 @@ import Notiflix from 'notiflix';
 
 const DemoProduct = (props) => {
 
-    useDocTitle('MLD | Molad e Konsult - Demo our products')
+    useDocTitle('Baker Prescott')
 
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
